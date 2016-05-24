039edcadfd1c1732c429ca1c834887baea0cf7bf
testtheksaya <yay@efasdf.asdf>
2016-05-24 00:40:28 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,485 @@
+jQuery(document).ready(function($){
+	//cache DOM elements
+	var projectsContainer = $('.cd-projects-container'),
+		projectsPreviewWrapper = projectsContainer.find('.cd-projects-previews'),
+		projectPreviews = projectsPreviewWrapper.children('li'),
+		projects = projectsContainer.find('.cd-projects'),
+		navigationTrigger = $('.cd-nav-trigger'),
+		navigation = $('.cd-primary-nav'),
+		//if browser doesn't support CSS transitions...
+		transitionsNotSupported = ( $('.no-csstransitions').length > 0);
+
+	var animating = false,
+		//will be used to extract random numbers for projects slide up/slide down effect
+		numRandoms = projects.find('li').length, 
+		uniqueRandoms = [];
+
+	//open project
+	projectsPreviewWrapper.on('click', 'a', function(event){
+		event.preventDefault();
+		if( animating == false ) {
+			animating = true;
+			navigationTrigger.add(projectsContainer).addClass('project-open');
+			openProject($(this).parent('li'));
+		}
+	});
+
+	navigationTrigger.on('click', function(event){
+		event.preventDefault();
+		
+		if( animating == false ) {
+			animating = true;
+			if( navigationTrigger.hasClass('project-open') ) {
+				//close visible project
+				navigationTrigger.add(projectsContainer).removeClass('project-open');
+				closeProject();
+			} else if( navigationTrigger.hasClass('nav-visible') ) {
+				//close main navigation
+				navigationTrigger.removeClass('nav-visible');
+				navigation.removeClass('nav-clickable nav-visible');
+				if(transitionsNotSupported) projectPreviews.removeClass('slide-out');
+				else slideToggleProjects(projectsPreviewWrapper.children('li'), -1, 0, false);
+			} else {
+				//open main navigation
+				navigationTrigger.addClass('nav-visible');
+				navigation.addClass('nav-visible');
+				if(transitionsNotSupported) projectPreviews.addClass('slide-out');
+				else slideToggleProjects(projectsPreviewWrapper.children('li'), -1, 0, true);
+			}
+		}	
+
+		if(transitionsNotSupported) animating = false;
+	});
+
+	//scroll down to project info
+	projectsContainer.on('click', '.scroll', function(){
+		projectsContainer.animate({'scrollTop':$(window).height()}, 500); 
+	});
+
+	//check if background-images have been loaded and show project previews
+	projectPreviews.children('a').bgLoaded({
+	  	afterLoaded : function(){
+	   		showPreview(projectPreviews.eq(0));
+	  	}
+	});
+
+	function showPreview(projectPreview) {
+		if(projectPreview.length > 0 ) {
+			setTimeout(function(){
+				projectPreview.addClass('bg-loaded');
+				showPreview(projectPreview.next());
+			}, 150);
+		}
+	}
+
+	function openProject(projectPreview) {
+		var projectIndex = projectPreview.index();
+		projects.children('li').eq(projectIndex).add(projectPreview).addClass('selected');
+		
+		if( transitionsNotSupported ) {
+			projectPreviews.addClass('slide-out').removeClass('selected');
+			projects.children('li').eq(projectIndex).addClass('content-visible');
+			animating = false;
+		} else { 
+			slideToggleProjects(projectPreviews, projectIndex, 0, true);
+		}
+	}
+
+	function closeProject() {
+		projects.find('.selected').removeClass('selected').on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+			$(this).removeClass('content-visible').off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+			slideToggleProjects(projectsPreviewWrapper.children('li'), -1, 0, false);
+		});
+
+		//if browser doesn't support CSS transitions...
+		if( transitionsNotSupported ) {
+			projectPreviews.removeClass('slide-out');
+			projects.find('.content-visible').removeClass('content-visible');
+			animating = false;
+		}
+	}
+
+	function slideToggleProjects(projectsPreviewWrapper, projectIndex, index, bool) {
+		if(index == 0 ) createArrayRandom();
+		if( projectIndex != -1 && index == 0 ) index = 1;
+
+		var randomProjectIndex = makeUniqueRandom();
+		if( randomProjectIndex == projectIndex ) randomProjectIndex = makeUniqueRandom();
+		
+		if( index < numRandoms - 1 ) {
+			projectsPreviewWrapper.eq(randomProjectIndex).toggleClass('slide-out', bool);
+			setTimeout( function(){
+				//animate next preview project
+				slideToggleProjects(projectsPreviewWrapper, projectIndex, index + 1, bool);
+			}, 150);
+		} else if ( index == numRandoms - 1 ) {
+			//this is the last project preview to be animated 
+			projectsPreviewWrapper.eq(randomProjectIndex).toggleClass('slide-out', bool).one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+				if( projectIndex != -1) {
+					projects.children('li.selected').addClass('content-visible');
+					projectsPreviewWrapper.eq(projectIndex).addClass('slide-out').removeClass('selected');
+				} else if( navigation.hasClass('nav-visible') && bool ) {
+					navigation.addClass('nav-clickable');
+				}
+				projectsPreviewWrapper.eq(randomProjectIndex).off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+				animating = false;
+			});
+		}
+	}
+
+	//http://stackoverflow.com/questions/19351759/javascript-random-number-out-of-5-no-repeat-until-all-have-been-used
+	function makeUniqueRandom() {
+	    var index = Math.floor(Math.random() * uniqueRandoms.length);
+	    var val = uniqueRandoms[index];
+	    // now remove that value from the array
+	    uniqueRandoms.splice(index, 1);
+	    return val;
+	}
+
+	function createArrayRandom() {
+		//reset array
+		uniqueRandoms.length = 0;
+		for (var i = 0; i < numRandoms; i++) {
+            uniqueRandoms.push(i);
+        }
+	}
+});
+
+ /*
+ * BG Loaded
+ * Copyright (c) 2014 Jonathan Catmull
+ * Licensed under the MIT license.
+ */
+ (function($){
+ 	$.fn.bgLoaded = function(custom) {
+	 	var self = this;
+
+		// Default plugin settings
+		var defaults = {
+			afterLoaded : function(){
+				this.addClass('bg-loaded');
+			}
+		};
+
+		// Merge default and user settings
+		var settings = $.extend({}, defaults, custom);
+
+		// Loop through element
+		self.each(function(){
+			var $this = $(this),
+				bgImgs = $this.css('background-image').split(', ');
+			$this.data('loaded-count',0);
+			$.each( bgImgs, function(key, value){
+				var img = value.replace(/^url\(["']?/, '').replace(/["']?\)$/, '');
+				$('<img/>').attr('src', img).load(function() {
+					$(this).remove(); // prevent memory leaks
+					$this.data('loaded-count',$this.data('loaded-count')+1);
+					if ($this.data('loaded-count') >= bgImgs.length) {
+						settings.afterLoaded.call($this);
+					}
+				});
+			});
+
+		});
+	};
+})(jQuery);
+
+/* Other stuff */
+// Array of all the questions and choices to populate the questions. This might be saved in some JSON file or a database and we would have to read the data in.
+var all_questions = [{
+  question_string: "What island is Ulysses from?",
+  choices: {
+    correct: "Ithaca",
+    wrong: ["Crete", "Rhodes", "Ogygia"]
+  }
+}, {
+  question_string: "Ulysses’s and his crew’s journey starts in what battle?",
+  choices: {
+    correct: "The Battle of Troy",
+    wrong: ["The Trojan War", "The Grecian War", "The War of Ithaca"]
+  }
+}, {
+  question_string: "Who is Ulysses married to?",
+  choices: {
+    correct: "Penelope",
+    wrong: ["Circe", "Calypso", "Medusa"]
+  }
+}, {
+  question_string: "What is the name of the Cyclops?",
+  choices: {
+    correct: "Polyphemus",
+    wrong: ["Bill", "Poseidon", "Naussica"]
+  }
+}, {
+  question_string: "What is the name Ulysses gives to the cyclope",
+  choices: {
+    correct: "Nobody",
+    wrong: ["Bill", "Joe", "Ulysses"]
+  }
+}, {
+  question_string: "What island did Ulysses end up on when they had no food?",
+  choices: {
+    correct: "Circe's Island",
+    wrong: ["Crete", "Rhodes", "Ogygia"]
+  }
+}, {
+  question_string: "What animal did the sorceress on that island try to turn the crew into?",
+  choices: {
+    correct: "Pigs",
+    wrong: ["Cows", "Sheep", "Ducks"]
+  }
+}, {
+  question_string: "When Ulysses said he had to leave where did the sorceress tell them to go?",
+  choices: {
+    correct: "Tartarus",
+    wrong: ["Heaven", "Mt. Olympus", "Ithaca"]
+  }
+}, {
+  question_string: "Ulysses has to choose between what two monsters?",
+  choices: {
+    correct: "Scylla and Charybdis",
+    wrong: ["Charybdis and Polyphemus", "Scylla and Polyphemus", "Poseidon and Amphitrite"]
+  }
+}, {
+  question_string: "Where does Ulysses end up after his ship is destroyed?",
+  choices: {
+    correct: "Ogygia",
+    wrong: ["Crete", "Troy", "Ithaca"]
+  }
+}, {
+  question_string: "Who is on the island he lands on after his ship is destroyed?",
+  choices: {
+    correct: "Calypso",
+    wrong: ["Circe", "Penelope", "Telemachus"]
+  }
+
+}, {
+  question_string: "Which god answers Ulysses’s plea for mercy?",
+  choices: {
+    correct: "Athena",
+    wrong: ["Zeus", "Poseidon", "Apollo"]
+  }
+}, {
+  question_string: "When he gets back home on his raft, what does he disguise himself as?",
+  choices: {
+    correct: "A beggar",
+    wrong: ["A rich merchant", "Telemachus", "Doesn't disguise himself"]
+  }
+}, {
+  question_string: "He tricks his wife into holding a competition of what do decide the successful suitor?",
+  choices: {
+    correct: "Archery",
+    wrong: ["Fighting", "Discus Throwing", "Running"]
+  }
+}, {
+  question_string: "Ulysses kills all the suitors, along with a few others. What causes him to get enraged?",
+  choices: {
+    correct: "His son's blood",
+    wrong: ["His son's battlecry", "His son's death", "His own blood"]
+  }
+}];
+
+// An object for a Quiz, which will contain Question objects.
+var Quiz = function(quiz_name) {
+  // Private fields for an instance of a Quiz object.
+  this.quiz_name = quiz_name;
+
+  // This one will contain an array of Question objects in the order that the questions will be presented.
+  this.questions = [];
+}
+
+// A function that you can enact on an instance of a quiz object. This function is called add_question() and takes in a Question object which it will add to the questions field.
+Quiz.prototype.add_question = function(question) {
+  // Randomly choose where to add question
+  var index_to_add_question = Math.floor(Math.random() * this.questions.length);
+  this.questions.splice(index_to_add_question, 0, question);
+}
+
+// A function that you can enact on an instance of a quiz object. This function is called render() and takes in a variable called the container, which is the <div> that I will render the quiz in.
+Quiz.prototype.render = function(container) {
+  // For when we're out of scope
+  var self = this;
+
+  // Hide the quiz results modal
+  $('#quiz-results').hide();
+
+  // Write the name of the quiz
+  $('#quiz-name').text(this.quiz_name);
+
+  // Create a container for questions
+  var question_container = $('<div>').attr('id', 'question').insertAfter('#quiz-name');
+
+  // Helper function for changing the question and updating the buttons
+  function change_question() {
+    self.questions[current_question_index].render(question_container);
+    $('#prev-question-button').prop('disabled', current_question_index === 0);
+    $('#next-question-button').prop('disabled', current_question_index === self.questions.length - 1);
+
+    // Determine if all questions have been answered
+    var all_questions_answered = true;
+    for (var i = 0; i < self.questions.length; i++) {
+      if (self.questions[i].user_choice_index === null) {
+        all_questions_answered = false;
+        break;
+      }
+    }
+    $('#submit-button').prop('disabled', !all_questions_answered);
+  }
+
+  // Render the first question
+  var current_question_index = 0;
+  change_question();
+
+  // Add listener for the previous question button
+  $('#prev-question-button').click(function() {
+    if (current_question_index > 0) {
+      current_question_index--;
+      change_question();
+    }
+  });
+
+  // Add listener for the next question button
+  $('#next-question-button').click(function() {
+    if (current_question_index < self.questions.length - 1) {
+      current_question_index++;
+      change_question();
+    }
+  });
+
+  // Add listener for the submit answers button
+  $('#submit-button').click(function() {
+    // Determine how many questions the user got right
+    var score = 0;
+    for (var i = 0; i < self.questions.length; i++) {
+      if (self.questions[i].user_choice_index === self.questions[i].correct_choice_index) {
+        score++;
+      }
+    }
+
+    // Display the score with the appropriate message
+    var percentage = score / self.questions.length;
+    console.log(percentage);
+    var message;
+    if (percentage === 1) {
+      message = 'Great job!'
+    } else if (percentage >= .75) {
+      message = 'You did alright.'
+    } else if (percentage >= .5) {
+      message = 'Better luck next time.'
+    } else {
+      message = 'Maybe you should try a little harder.'
+    }
+    $('#quiz-results-message').text(message);
+    $('#quiz-results-score').html('You got <b>' + score + '/' + self.questions.length + '</b> questions correct.');
+    $('#quiz-results').slideDown();
+    $('#quiz button').slideUp();
+  });
+
+  // Add a listener on the questions container to listen for user select changes. This is for determining whether we can submit answers or not.
+  question_container.bind('user-select-change', function() {
+    var all_questions_answered = true;
+    for (var i = 0; i < self.questions.length; i++) {
+      if (self.questions[i].user_choice_index === null) {
+        all_questions_answered = false;
+        break;
+      }
+    }
+    $('#submit-button').prop('disabled', !all_questions_answered);
+  });
+}
+
+// An object for a Question, which contains the question, the correct choice, and wrong choices. This block is the constructor.
+var Question = function(question_string, correct_choice, wrong_choices) {
+  // Private fields for an instance of a Question object.
+  this.question_string = question_string;
+  this.choices = [];
+  this.user_choice_index = null; // Index of the user's choice selection
+
+  // Random assign the correct choice an index
+  this.correct_choice_index = Math.floor(Math.random() * wrong_choices.length + 1);
+
+  // Fill in this.choices with the choices
+  var number_of_choices = wrong_choices.length + 1;
+  for (var i = 0; i < number_of_choices; i++) {
+    if (i === this.correct_choice_index) {
+      this.choices[i] = correct_choice;
+    } else {
+      // Randomly pick a wrong choice to put in this index
+      var wrong_choice_index = Math.floor(Math.random(0, wrong_choices.length));
+      this.choices[i] = wrong_choices[wrong_choice_index];
+
+      // Remove the wrong choice from the wrong choice array so that we don't pick it again
+      wrong_choices.splice(wrong_choice_index, 1);
+    }
+  }
+}
+
+// A function that you can enact on an instance of a question object. This function is called render() and takes in a variable called the container, which is the <div> that I will render the question in. This question will "return" with the score when the question has been answered.
+Question.prototype.render = function(container) {
+  // For when we're out of scope
+  var self = this;
+
+  // Fill out the question label
+  var question_string_h2;
+  if (container.children('h2').length === 0) {
+    question_string_h2 = $('<h2>').appendTo(container);
+  } else {
+    question_string_h2 = container.children('h2').first();
+  }
+  question_string_h2.text(this.question_string);
+
+  // Clear any radio buttons and create new ones
+  if (container.children('input[type=radio]').length > 0) {
+    container.children('input[type=radio]').each(function() {
+      var radio_button_id = $(this).attr('id');
+      $(this).remove();
+      container.children('label[for=' + radio_button_id + ']').remove();
+    });
+  }
+  for (var i = 0; i < this.choices.length; i++) {
+    // Create the radio button
+    var choice_radio_button = $('<input>')
+      .attr('id', 'choices-' + i)
+      .attr('type', 'radio')
+      .attr('name', 'choices')
+      .attr('value', 'choices-' + i)
+      .attr('checked', i === this.user_choice_index)
+      .appendTo(container);
+
+    // Create the label
+    var choice_label = $('<label>')
+      .text(this.choices[i])
+      .attr('for', 'choices-' + i)
+      .appendTo(container);
+  }
+
+  // Add a listener for the radio button to change which one the user has clicked on
+  $('input[name=choices]').change(function(index) {
+    var selected_radio_button_value = $('input[name=choices]:checked').val();
+
+    // Change the user choice index
+    self.user_choice_index = parseInt(selected_radio_button_value.substr(selected_radio_button_value.length - 1, 1));
+
+    // Trigger a user-select-change
+    container.trigger('user-select-change');
+  });
+}
+
+// "Main method" which will create all the objects and render the Quiz.
+$(document).ready(function() {
+  // Create an instance of the Quiz object
+  var quiz = new Quiz('Ulysses Quiz');
+
+  // Create Question objects from all_questions and add them to the Quiz object
+  for (var i = 0; i < all_questions.length; i++) {
+    // Create a new Question object
+    var question = new Question(all_questions[i].question_string, all_questions[i].choices.correct, all_questions[i].choices.wrong);
+
+    // Add the question to the instance of the Quiz object that we created previously
+    quiz.add_question(question);
+  }
+
+  // Render the quiz
+  var quiz_container = $('#quiz');
+  quiz.render(quiz_container);
+});
